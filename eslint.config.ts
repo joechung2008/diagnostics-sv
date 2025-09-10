32aa3227a531b575e25f466cce20c0f65b1754f8
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-10 15:50:32 -0400

    Remove globals package

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,6 @@
 import { includeIgnoreFile } from "@eslint/compat";
 import prettier from "eslint-config-prettier";
 import svelte from "eslint-plugin-svelte";
-import globals from "globals";
 import { fileURLToPath } from "node:url";
 import ts from "typescript-eslint";
 import svelteConfig from "./svelte.config.js";
@@ -18,9 +17,6 @@ export default ts.config(
   prettier,
   ...svelte.configs.prettier,
   {
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
-    },
     rules: {
       // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
       // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
